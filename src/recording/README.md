b8df7d1f1de90205a1043a1e120c21b234822562
Robin <44874951+r-grun@users.noreply.github.com>
2022-03-15 11:22:49 +0100

    Add recording

--- src/recording/README.md
+++ src/recording/README.md
@@ -1,6 +1,6 @@
 # Instructions on how to use recording functionality
 
-The script will record the audio loopback (of the speakers and microphone-input) and store it in files of 6s length to `./recordings`
+The script will record the audio loopback (of the speakers and microphone-input)  as `.wav` file and store it in files of 6s length to `./recordings`
 
 <br>
 
@@ -9,17 +9,20 @@ The script will record the audio loopback (of the speakers and microphone-input)
 1. Make sure ALSA is installed on the system
 2. Activate `snd-aloop` kernel moduke for setting up loopback devices <br>
     `$ sudo modprobe snd-aloop` <br>
-    To activate the module on startup, insert it in `/etc/modules`
+    `$ arecord -l` will provide a list of the installed soundcards.<br>
+    To activate the module on startup, insert it in `/etc/modules`.<br>
+    The loopback card will then be card 0.
 3. Install `pyalsaaudio` python module <br>
     `$ pip install pyalsaaudio`
 
 <br>
 
-## Running the script 
+## Start recording and running the script 
 
 
-1. Start `recording.py` script <br>
-    `$ python3 ./recording.py`
+2. Start `recording.py` script <br>
+    `$ python3 ./recording.py [-d <device>]`<br>
+    A specific device can be omitted by passing it via the parameter `-d`
 
 
 
---
+++ src/recording/recording.py
@@ -0,0 +1,61 @@
+import sys
+import time
+import getopt
+import wave
+import alsaaudio
+
+path = './recordings/'
+rec_file = 'rec.raw'
+conv_file = 'rec.wav'
+
+
+def usage():
+	print('usage: recordtest.py [-d <device>]', file=sys.stderr)
+	sys.exit(2)
+
+
+if __name__ == '__main__':
+
+	device = 'default'
+
+	opts, args = getopt.getopt(sys.argv[1:], 'd:')
+	for o, a in opts:
+		if o == '-d':
+			device = a
+
+	if not args:
+		usage()
+
+	f = open(path + rec_file, 'wb')
+
+	# Open the device in nonblocking capture mode in mono, with a sampling rate of 44100 Hz
+	# and 16 bit little endian samples
+	# The period size controls the internal number of frames per period.
+	# The significance of this parameter is documented in the ALSA api.
+	# For our purposes, it is suficcient to know that reads from the device
+	# will return this many frames. Each frame being 2 bytes long.
+	# This means that the reads below will return either 320 bytes of data
+	# or 0 bytes of data. The latter is possible because we are in nonblocking
+	# mode.
+	inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, channels=1,
+	                    rate=44100, format=alsaaudio.PCM_FORMAT_S16_LE, periodsize=160, device=device)
+
+    while True:
+
+        loops = 6000
+        while loops > 0:
+            loops -= 1
+            # Read data from device
+            l, data = inp.read()
+
+            if l:
+                f.write(data)
+                time.sleep(.001)
+        
+        with open(path + rec_file, "rb") as inp_f:
+            data_conv = inp_f.read()
+            with wave.open(conv_file, "wb") as out_f:
+                out_f.setnchannels(1)
+                out_f.setsampwidth(2) # number of bytes
+                out_f.setframerate(44100)
+                out_f.writeframesraw(data_conv)
