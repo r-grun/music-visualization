1d7843716168ba59e6141405d505448eece6a5b6
Robin <44874951+r-grun@users.noreply.github.com>
2022-05-05 08:57:35 +0200

    Change LED Channel

--- src/animating/animator.py
+++ src/animating/animator.py
@@ -20,7 +20,7 @@ LED_FREQ_HZ = 800000    # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10            # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False      # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 3         # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL = 1         # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 
